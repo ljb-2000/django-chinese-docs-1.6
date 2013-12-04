05142c5b7a9e1f22e3aaf192d7b99ef7bbd2b52e
borisliu <liulijun@263.net>
2013-12-04 15:01:02 +0800

    reinit

--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,10 @@
-.. Django documentation master file, created by
-   sphinx-quickstart on Wed Dec  4 14:29:57 2013.
+.. ChineseDjangoDocs documentation master file, created by
+   sphinx-quickstart on Wed Dec  4 15:00:37 2013.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Django's documentation!
-==================================
+Welcome to ChineseDjangoDocs's documentation!
+=============================================
 
 Contents:
 
--- docs/make.bat
+++ docs/make.bat
@@ -6,7 +6,8 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=_build
-set ALLSPHINXOPTS=-n -d %BUILDDIR%/doctrees %SPHINXOPTS% .
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
+set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
 	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
@@ -29,7 +30,7 @@ if "%1" == "help" (
 	echo.  latex      to make LaTeX files, you can set PAPER=a4 or PAPER=letter
 	echo.  text       to make text files
 	echo.  man        to make manual pages
-	echo.  texinfo    to make a Texinfo source file
+	echo.  texinfo    to make Texinfo files
 	echo.  gettext    to make PO message catalogs
 	echo.  changes    to make an overview over all changed/added/deprecated items
 	echo.  linkcheck  to check all external links for integrity
@@ -44,7 +45,7 @@ if "%1" == "clean" (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b djangohtml %ALLSPHINXOPTS% %BUILDDIR%/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
@@ -98,9 +99,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\django.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\ChineseDjangoDocs.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\django.qhc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\ChineseDjangoDocs.ghc
 	goto end
 )
 
@@ -152,7 +153,7 @@ if "%1" == "texinfo" (
 	goto end
 )
 
-if "%%1" == "gettext" (
+if "%1" == "gettext" (
 	%SPHINXBUILD% -b gettext %I18NSPHINXOPTS% %BUILDDIR%/locale
 	if errorlevel 1 exit /b 1
 	echo.
